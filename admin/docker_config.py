291cce54c77e7128d264110f973d889adc6e5173
miutaku <miutaku@miutakuMacBook-Pro.local>
2025-02-20 02:51:25 +0900

    https://www.e-lab.tokyo/blog/2024/10/03/powerdns%E3%81%A7%E3%82%AD%E3%83%A3%E3%83%83%E3%82%B7%E3%83%A5dns%E3%81%A8%E6%A8%A9%E5%A8%81dns%E3%82%92docker%E3%81%A7%E6%A7%8B%E7%AF%89%E3%81%99%E3%82%8B/ を参考にした

--- admin/docker_config.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY = '__PDNS_ADMIN_SECRET_KEY__' # python3 -c 'import secrets; print(secrets.token_hex())'
-PORT = 80
-SQLALCHEMY_DATABASE_URI = 'postgresql://__PSQL_PDNS_ADMIN__:__PSQL_PDNS_ADMIN_PASSWORD__@__PSQL_PDNS_ADMIN_HOST__:__PSQL_PDNS_ADMIN_PORT__/__PSQL_PDNS_ADMIN_DB__?sslmode=verify-full'
